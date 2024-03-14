d64e8ae18bd8edd2267710450909db3dc1ccdb41
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-14 12:32:22 +0100

    bumped prisma version and updated endpoint

--- src/app/api/v1/extension/route.ts
+++ src/app/api/v1/extension/route.ts
@@ -1,9 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/../lib/prisma';
 
 type ResponseData = {
   message: string
 }
 
 export const GET = (req: NextRequest, res: NextResponse<ResponseData>) => {
-    return NextResponse.json({ message: 'Hello from Next.js!' }, { status: 200 })
+    const queryParams = new URLSearchParams(req.nextUrl.search);
+
+    if(!queryParams.has("id")) {
+      return NextResponse.json({ message: 'Missing "id" parameter!' }, { status: 400 })
+    }
+
+    const extension = prisma.extension.findUnique({
+      where: {id: queryParams.get("id")!},
+      include: {
+        developer: {
+          select: {name: true}
+        }
+      }
+    })
+
+    return NextResponse.json(extension, { status: 200 })
 }

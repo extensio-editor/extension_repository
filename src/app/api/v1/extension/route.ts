85b6bb1d997d33631924897977902d6f4f9c69b0
Bread <77288737+riley0122@users.noreply.github.com>
2024-03-16 20:30:24 +0100

    Added some validation

--- src/app/api/v1/extension/route.ts
+++ src/app/api/v1/extension/route.ts
@@ -41,7 +41,8 @@ export const POST = async (req: NextRequest, res: NextResponse<ResponseData>) =>
     where: {email: session["user"]!["email"]!},
     select: {
       id: true,
-      extensions: true
+      extensions: true,
+      name: true,
     }
   })
 
@@ -56,10 +57,44 @@ export const POST = async (req: NextRequest, res: NextResponse<ResponseData>) =>
     return NextResponse.json({ message: "Missing parameter. Required parameters are 'name' & 'gitUrl'" }, { status: 400 });
   }
 
+  // Validate the extension name
+  // Extention name can not contain "/" because it will be used to split between users.
+  // Extension name can also not contain any url unsafe characters.
+  let extensionName = body.get("name")! as string;
+  extensionName = encodeURIComponent(extensionName);
+
+  // Check if there is not already an extension registered for the same url
+  const sameUrlExtension = await prisma.extension.findFirst({
+    where: {gitUrl: body.get("gitUrl")! as string},
+    select: {
+      id: true
+    }
+  })
+
+  if (sameUrlExtension) return NextResponse.json({ message: `There already is an extension for this url! It has id ${sameUrlExtension.id}` }, { status: 409 })
+
+  // Check if there is already an extension by that name
+  // If there is rename it to be [username]/[name]
+  // If the user already has an extension with the same name refuse it.
+  const existingExtention = await prisma.extension.findFirst({
+    where: {name: extensionName},
+    select: {
+      developerId: true,
+    }
+  })
+
+  if(existingExtention) {
+    if (existingExtention.developerId == user.id) {
+      return NextResponse.json({ message: "Can only have one extention with name per user!" }, { status: 405 });
+    } else {
+      extensionName = encodeURIComponent(user.name!);
+    }
+  }
+
   try {
     const extension = await prisma.extension.create({
       data: {
-        name: body.get("name")! as string,
+        name: extensionName,
         gitUrl: body.get("gitUrl")! as string,
         developer: {
           connect: {

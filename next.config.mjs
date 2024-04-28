b78bbcf7350e15d0dbbe89b4d91db51af5f58ffb
Bread <77288737+riley0122@users.noreply.github.com>
2024-04-28 19:16:20 +0200

    Revert "Added sentry monitoring"
    This reverts commit 86195ed5f5afdea55a3b251afc19dcac37c2c23e.

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,4 @@
-import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withSentryConfig(nextConfig, {
-silent: true,
-org: "extensio-editor",
-project: "extension-repository",
-}, {
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Transpiles SDK to be compatible with IE11 (increases bundle size)
-transpileClientSDK: true,
-
-// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
-
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors.
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+export default nextConfig;
